a480f70e239d425bd42fc8a0f642dd1a4f83af88
Milk T <milkt2021@gmail.com>
2023-10-25 11:25:38 +0200

    inserted about us text

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 function AboutUs() {
   return (
-    <div>AboutUs</div>
-  )
+    <div>
+      AboutUs 
+      Nestled in the heart of the city, our establishment offers an
+      enchanting dining experience that transcends the ordinary. With a vibrant
+      and whimsical ambiance, Circus Restaurant invites guests to step into a
+      world of wonder. Our menu is a carefully curated blend of global cuisines,
+      showcasing the finest ingredients and culinary artistry. From delectable
+      appetizers to decadent desserts, every dish is a masterpiece that
+      tantalizes the taste buds. But the magic doesn't stop there; we feature
+      live performances and themed events, making every visit a celebration. At
+      Circus Restaurant, we create unforgettable memories one plate and one act
+      at a time. Join us for a truly unique and unforgettable dining adventure.
+    </div>
+  );
 }
 
-export default AboutUs
+export default AboutUs;
