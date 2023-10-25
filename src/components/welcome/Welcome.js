417f193cd42446efc4297d4c97114df6837ad966
dom-arm <118741935+dom-arm@users.noreply.github.com>
2023-10-25 15:05:53 +0200

    Clean up and renaming

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -3,8 +3,8 @@ import "./welcome.css";
 
 function Welcome() {
   return (
-    <section>
-      <article class="welcome">
+    <section class="welcome-section">
+      <article>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
