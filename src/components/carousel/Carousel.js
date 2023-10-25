d7e38cf69e9663893dbcf53abe7d5d0bab5f47a1
dom-arm <118741935+dom-arm@users.noreply.github.com>
2023-10-25 17:02:09 +0200

    Carousel created

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import "./carousel.css";
+import CarouselTemplate from "react-bootstrap/Carousel";
+import acrobats from "../../assets/acrobats.jpg";
+import circusText from "../../assets/circus-text.jpg";
+import clown from "../../assets/face-paint-clown.jpg";
 
 function Carousel() {
   return (
-    <div>Carousel</div>
-  )
+    <section class="section-carousel">
+      <CarouselTemplate fade>
+        <CarouselTemplate.Item>
+          <img class="slide-img" src={acrobats} alt="Acrobats"></img>
+          <CarouselTemplate.Caption>
+            <h3>3 of november</h3>
+            <p>CIRCUS!</p>
+          </CarouselTemplate.Caption>
+        </CarouselTemplate.Item>
+        <CarouselTemplate.Item>
+          <img
+            class="slide-img"
+            src={circusText}
+            alt="Letters that reads 'Circus'"
+          ></img>
+          <CarouselTemplate.Caption>
+            <h3>4 of november</h3>
+            <p>CIRCUS AGAIN!!</p>
+          </CarouselTemplate.Caption>
+        </CarouselTemplate.Item>
+        <CarouselTemplate.Item>
+          <img
+            class="slide-img"
+            src={clown}
+            alt="Person with her face painted clown-style"
+          ></img>
+          <CarouselTemplate.Caption>
+            <h3>10 of november</h3>
+            <p>ANOTHER CIRCUS!!!</p>
+          </CarouselTemplate.Caption>
+        </CarouselTemplate.Item>
+      </CarouselTemplate>
+    </section>
+  );
 }
 
-export default Carousel
+export default Carousel;
