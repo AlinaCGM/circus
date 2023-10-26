f7f348d9b10903121b25ae6c8951029f1436a443
Alina <alinbogz@gmail.com>
2023-10-26 21:13:40 +0200

    about responsive done

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "../../components/hero/Hero";
 import "./home.css";
 import Welcome from "../../components/welcome/Welcome";
-// import RestaurantComp from "../../components/restaurantComp/RestaurantComp";
+import RestaurantComp from "../../components/restaurantComp/RestaurantComp";
 import AboutUs from "../../components/aboutUs/AboutUs";
 import Carousel from "../../components/carousel/Carousel";
 import Footer from "../../components/footer/Footer";
@@ -12,7 +12,7 @@ function Home() {
     <div className="home-container">
       <Hero />
       <Welcome />
-      {/* <RestaurantComp /> */}
+      <RestaurantComp />
       <AboutUs />
       <Carousel />
       <Footer />
