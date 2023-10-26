357d11d6e0935c6873428e315af3d2c0ff8fdcbc
Alina <alinbogz@gmail.com>
2023-10-26 15:06:55 +0200

    one more section added in About component

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -1,53 +1,81 @@
 import React from "react";
 import "./aboutUs.css";
-import homegirlsImage from "../../assets/homegirls.png";
+import homegirlsImage from "../../assets/2.jpg";
+import homegirlsImage1 from "../../assets/1.jpg";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function AboutUs() {
   return (
-    <div className="container d-flex justify-content-center align-items-center ">
-      <div className="jumbotron jumbotron-fluid">
-        <main>
-          <div className="row">
-            <header>
-              <h2>About Us</h2>
-            </header>
+    <div className="container-about ">
+      <h2 className="title-about">About Us</h2>
+
+      <div className="row ">
+        <div
+          className=" col-sm-10 col-lg-5 "
+          style={{ backgroundColor: "#343438" }}
+        >
+          <p className="content-about">
+            Located in the heart of the city, our establishment offers an
+            enchanting dining experience that transcends the ordinary. With a
+            vibrant and whimsical ambiance, Star 5 Restaurant invites guests to
+            step into a world of wonder. <br />
+            <br />
+            Our menu is a carefully curated blend of global cuisines, showcasing
+            the finest ingredients and culinary artistry. Every dish is a
+            masterpiece that tantalizes the taste buds. <br />
+            <br /> But the magic doesn't stop there; we feature live
+            performances and themed events, making every visit a celebration.
+            <br />
+            <br />
+            Whether you're here for a romantic dinner or a special event, every
+            visit to Star 5 Restaurant is a celebration. We're here to create
+            unforgettable memories, one plate and one act at a time. Join us for
+            a truly unique and unforgettable dining adventure that will stay
+            with you long after you leave our doors.
+          </p>
+        </div>
+        <div className="col-lg-6 ">
+          <img
+            width="100%"
+            src={homegirlsImage}
+            alt="Homegirls"
+            className="img-fluid"
+          />
+        </div>
+      </div>
+
+      <div>
+        <div className="row">
+          <div className="col-lg-6 ">
+            <img
+              width="100%"
+              src={homegirlsImage1}
+              alt="Homegirls"
+              className="img-fluid"
+            />
           </div>
-          <div>
-            <div className="row">
-              <div className="col-lg-6" style={{ backgroundColor: "#343438" }}>
-                <p>
-                  Located in the heart of the city, our establishment offers an
-                  enchanting dining experience that transcends the ordinary.
-                  With a vibrant and whimsical ambiance, Star 5 Restaurant
-                  invites guests to step into a world of wonder.{" "}
-                  <br />
-                  <br />
-                  Our menu is a carefully curated blend of global cuisines,
-                  showcasing the finest ingredients and culinary artistry. Every
-                  dish is a masterpiece that tantalizes the taste buds. <br />
-                  <br /> But the magic doesn't stop there; we feature live
-                  performances and themed events, making every visit a
-                  celebration. 
-                  <br /><br />Whether you're here for a romantic dinner or a special event,
-                  every visit to Star 5 Restaurant is a celebration.
-                  We're here to create unforgettable memories, one plate and one
-                  act at a time. Join us for a truly unique and unforgettable
-                  dining adventure that will stay with you long after you leave
-                  our doors.
-                </p>
-              </div>
-              <div className="col-lg-6" >
-                <img
-                  src={homegirlsImage}
-                  alt="Homegirls"
-                  className="img-fluid"
-                  style={{ height: "540px", width: "600px"}}
-                />
-              </div>
-            </div>
+          <div className="col-lg-6 " style={{ backgroundColor: "#343438" }}>
+            <p className="content-about">
+              Located in the heart of the city, our establishment offers an
+              enchanting dining experience that transcends the ordinary. With a
+              vibrant and whimsical ambiance, Star 5 Restaurant invites guests
+              to step into a world of wonder. <br />
+              <br />
+              Our menu is a carefully curated blend of global cuisines,
+              showcasing the finest ingredients and culinary artistry. Every
+              dish is a masterpiece that tantalizes the taste buds. <br />
+              <br /> But the magic doesn't stop there; we feature live
+              performances and themed events, making every visit a celebration.
+              <br />
+              <br />
+              Whether you're here for a romantic dinner or a special event,
+              every visit to Star 5 Restaurant is a celebration. We're here to
+              create unforgettable memories, one plate and one act at a time.
+              Join us for a truly unique and unforgettable dining adventure that
+              will stay with you long after you leave our doors.
+            </p>
           </div>
-        </main>
+        </div>
       </div>
     </div>
   );
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,18 +2,17 @@ import React from "react";
 import Hero from "../../components/hero/Hero";
 import "./home.css";
 import Welcome from "../../components/welcome/Welcome";
-import RestaurantComp from "../../components/restaurantComp/RestaurantComp";
+// import RestaurantComp from "../../components/restaurantComp/RestaurantComp";
 import AboutUs from "../../components/aboutUs/AboutUs";
 import Carousel from "../../components/carousel/Carousel";
 import Footer from "../../components/footer/Footer";
-
 
 function Home() {
   return (
     <div className="home-container">
       <Hero />
       <Welcome />
-      <RestaurantComp />
+      {/* <RestaurantComp /> */}
       <AboutUs />
       <Carousel />
       <Footer />
