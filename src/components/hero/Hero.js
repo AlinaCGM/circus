4778dd2a1906fd20b698c0505bc1e064147445da
Alina <alinbogz@gmail.com>
2023-10-27 13:14:15 +0200

    booking window created

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/logo.png";
 import "./hero.css";
+import Booking from "../booking/Booking";
 
 function Hero() {
+  const [isBookingOpen, setBookingOpen] = useState(false);
+
+  const openBookingPopup = () => {
+    setBookingOpen(true);
+  };
+
+  const closeBookingPopup = () => {
+    setBookingOpen(false);
+  };
+
   return (
     <div className="container-hero">
       <div className="title-div">
@@ -12,11 +23,19 @@ function Hero() {
 
         <img width="220px" height="auto" src={logo} alt="logo" />
         <div className="cta-div mt-2">
-          <a href="#" className="cta-button">
-            Discover More
-          </a>
+          <div className=" btn-hero">
+            <a href="#" className="cta-button">
+              Discover More
+            </a>
+          </div>
+          <div className="mt-4 btn-hero">
+            <a href="#" className="cta-button" onClick={openBookingPopup}>
+              Book
+            </a>
+          </div>
         </div>
       </div>
+      {isBookingOpen && <Booking closePopup={closeBookingPopup} />}
     </div>
   );
 }
